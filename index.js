3d1c0002e44123d28652ddd9c96e3373f11dcb96
Saajvatz <ajvatzalejandro@gmail.com>
2025-01-31 10:27:12 -0600

    a

---
+++ index.js
@@ -0,0 +1,22 @@
+let DOM = document.querySelector("#root");
+
+let contenedor = document.createElement('div');
+
+let footer = document.createElement("div");
+contenedor.appendChild(footer);
+footer.className = "div-footer"
+
+let progreso = document.createElement("div");
+contenedor.appendChild(progreso);
+progreso.className = "div-progreso";
+
+let tablero = document.createElement("div");
+contenedor.appendChild(tablero);
+tablero.className = "div-tablero";
+
+let header = document.createElement("div");
+contenedor.appendChild(header);
+header.className = "div-header";
+
+DOM.appendChild(contenedor);
+contenedor.className = "div-contenedor";
