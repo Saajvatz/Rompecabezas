e734ca0364434c9423a0649fd7061cdee59b6432
Saajvatz <ajvatzalejandro@gmail.com>
2025-02-13 17:33:19 -0600

    vuelta de carta

--- componentes/tablero/tablero.js
+++ componentes/tablero/tablero.js
@@ -1,23 +1,37 @@
 import { todas_las_cartas } from "./data.js";
-function item(contenido){
+
+function item(contenido) {
     let div = document.createElement('div');
     div.className = "carta";
-    div.textContent = contenido;
-
+    
+    let frente = document.createElement("div");
+    frente.className = "frente";
+    frente.textContent = "?"; 
+    
+    let dorso = document.createElement("div");
+    dorso.className = "dorso";
+    dorso.textContent = contenido; 
+    
+    div.appendChild(frente);
+    div.appendChild(dorso);
+    
+    div.addEventListener("click", () => {
+        div.classList.toggle("volteada");
+    });
+    
     return div;
 }
 
-function cargarCartas(){
+
+
+
+function cargarCartas() {
     let div = document.createElement('div');
     div.className = "div-tablero";
-    todas_las_cartas.forEach((letra)=>{
+    todas_las_cartas.forEach((letra) => {
         div.appendChild(item(letra));
     });
-
-
     return div;
 }
 
-export { cargarCartas } 
-
-   
+export { cargarCartas };
